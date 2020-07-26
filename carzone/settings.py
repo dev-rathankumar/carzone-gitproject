43ceeebdaeec870a9db2b70ecf406279c2ebf923
Rathan <rathan.kumar492@gmail.com>
2020-07-26 14:36:45 +0530

    added allowed host

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['floating-badlands-41165.herokuapp.com', 'carzoneapp.co', 'www.carzoneapp.co']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
